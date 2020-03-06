9934bc77060a1ffc8367664a057038eb885bc621
Ahmed Maruf <maruf@cefalo.com>
2020-03-06 12:12:57 +0600

    Create API to consume and return data

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Story;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware( 'auth:api' )->get( '/user', function ( Request $request ) {
     return $request->user();
+} );
+
+Route::get( '/story/create', function ( Request $request ) {
+} );
+
+Route::post( '/stories', function ( Request $request ) {
+    $attr = $request->validate( [
+        'title' => 'required',
+        'body' => 'required'
+    ]);
+    $story = Story::create($attr);
+
+    return response()->json($story, 201);
 });
